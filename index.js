bcaf9d1ae3220460f93061e355b65709ef867414
Jasmine Wynn <jasminewynn95@gmail.com>
2017-09-19 04:03:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var name = "John"
+var name = "Susan"
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
@@ -8,4 +8,3 @@ var height = "74"
 // Don't worry about this
 module.exports = { name, height
 }
-
